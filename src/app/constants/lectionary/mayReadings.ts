2412d7d2ee896c109fcb411be08f5673dd410386
Philippe <philippe.dixon@protonmail.com>
2025-01-12 21:37:08 -0500

    Update May readings to use new interface

--- src/app/constants/lectionary/mayReadings.ts
+++ src/app/constants/lectionary/mayReadings.ts
@@ -5,11 +5,12 @@ export const mayReadings: Month = {
 	name: "May",
 	days: [
 		{
+			ofMonth: 1,
 			reading_1: [
 				{
 					bookId: BookId.Samuel1,
-					chapters: "2",
-					verses: "1-10",
+					chapters: { first: 2, last: 2 },
+					verses: { first: 1, last: 10 },
 				},
 			],
 			reading_2: [
@@ -22,51 +23,89 @@ export const mayReadings: Month = {
 			],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "121" }],
-			reading_2: [{ bookId: BookId.Hebrews, chapters: "1-4" }],
+			ofMonth: 2,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 121, last: 121 } },
+			],
+			reading_2: [{ bookId: BookId.Hebrews, chapters: { first: 1, last: 4 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "122" }],
-			reading_2: [{ bookId: BookId.Hebrews, chapters: "5-7" }],
+			ofMonth: 3,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 122, last: 122 } },
+			],
+			reading_2: [{ bookId: BookId.Hebrews, chapters: { first: 5, last: 7 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "123" }],
-			reading_2: [{ bookId: BookId.Hebrews, chapters: "8-10" }],
+			ofMonth: 4,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 123, last: 123 } },
+			],
+			reading_2: [{ bookId: BookId.Hebrews, chapters: { first: 8, last: 10 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "124" }],
-			reading_2: [{ bookId: BookId.Hebrews, chapters: "11-13" }],
+			ofMonth: 5,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 124, last: 124 } },
+			],
+			reading_2: [
+				{ bookId: BookId.Hebrews, chapters: { first: 11, last: 13 } },
+			],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "125" }],
-			reading_2: [{ bookId: BookId.James, chapters: "1-3" }],
+			ofMonth: 6,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 125, last: 125 } },
+			],
+			reading_2: [{ bookId: BookId.James, chapters: { first: 1, last: 3 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "126" }],
-			reading_2: [{ bookId: BookId.James, chapters: "4-5" }],
+			ofMonth: 7,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 126, last: 126 } },
+			],
+			reading_2: [{ bookId: BookId.James, chapters: { first: 4, last: 5 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "127" }],
-			reading_2: [{ bookId: BookId.Peter1, chapters: "1-2" }],
+			ofMonth: 8,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 127, last: 127 } },
+			],
+			reading_2: [{ bookId: BookId.Peter1, chapters: { first: 1, last: 2 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "128" }],
-			reading_2: [{ bookId: BookId.Peter1, chapters: "3-5" }],
+			ofMonth: 9,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 128, last: 128 } },
+			],
+			reading_2: [{ bookId: BookId.Peter1, chapters: { first: 3, last: 5 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "129" }],
+			ofMonth: 10,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 129, last: 129 } },
+			],
 			reading_2: [{ bookId: BookId.Peter2 }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "130" }],
-			reading_2: [{ bookId: BookId.John1, chapters: "1-3" }],
+			ofMonth: 11,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 130, last: 130 } },
+			],
+			reading_2: [{ bookId: BookId.John1, chapters: { first: 1, last: 3 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "131" }],
-			reading_2: [{ bookId: BookId.John1, chapters: "4-5" }],
+			ofMonth: 12,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 131, last: 131 } },
+			],
+			reading_2: [{ bookId: BookId.John1, chapters: { first: 4, last: 5 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "132" }],
+			ofMonth: 13,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 132, last: 132 } },
+			],
 			reading_2: [
 				{ bookId: BookId.John2 },
 				{ bookId: BookId.John3 },
@@ -74,82 +113,134 @@ export const mayReadings: Month = {
 			],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "133" }],
-			reading_2: [{ bookId: BookId.John, chapters: "1-2" }],
+			ofMonth: 14,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 133, last: 133 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 1, last: 2 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "134" }],
-			reading_2: [{ bookId: BookId.John, chapters: "3-4" }],
+			ofMonth: 15,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 134, last: 134 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 3, last: 4 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "135" }],
-			reading_2: [{ bookId: BookId.John, chapters: "5-6" }],
+			ofMonth: 16,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 135, last: 135 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 5, last: 6 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "136" }],
-			reading_2: [{ bookId: BookId.John, chapters: "7-8" }],
+			ofMonth: 17,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 136, last: 136 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 7, last: 8 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "137" }],
-			reading_2: [{ bookId: BookId.John, chapters: "9-10" }],
+			ofMonth: 18,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 137, last: 137 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 9, last: 10 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "138" }],
-			reading_2: [{ bookId: BookId.John, chapters: "11-12" }],
+			ofMonth: 19,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 138, last: 138 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 11, last: 12 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "139" }],
-			reading_2: [{ bookId: BookId.John, chapters: "13-14" }],
+			ofMonth: 20,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 139, last: 139 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 13, last: 14 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "140" }],
-			reading_2: [{ bookId: BookId.John, chapters: "15-16" }],
+			ofMonth: 21,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 140, last: 140 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 15, last: 16 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "141" }],
-			reading_2: [{ bookId: BookId.John, chapters: "17-18" }],
+			ofMonth: 22,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 141, last: 141 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 17, last: 18 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "142" }],
-			reading_2: [{ bookId: BookId.John, chapters: "19" }],
+			ofMonth: 23,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 142, last: 142 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 19, last: 19 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "143" }],
-			reading_2: [{ bookId: BookId.John, chapters: "20-21" }],
+			ofMonth: 24,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 143, last: 143 } },
+			],
+			reading_2: [{ bookId: BookId.John, chapters: { first: 20, last: 21 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "144" }],
-			reading_2: [{ bookId: BookId.Acts, chapters: "1-2" }],
+			ofMonth: 25,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 144, last: 144 } },
+			],
+			reading_2: [{ bookId: BookId.Acts, chapters: { first: 1, last: 2 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "145" }],
-			reading_2: [{ bookId: BookId.Acts, chapters: "3-4" }],
+			ofMonth: 26,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 145, last: 145 } },
+			],
+			reading_2: [{ bookId: BookId.Acts, chapters: { first: 3, last: 4 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "146" }],
-			reading_2: [{ bookId: BookId.Acts, chapters: "5-6" }],
+			ofMonth: 27,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 146, last: 146 } },
+			],
+			reading_2: [{ bookId: BookId.Acts, chapters: { first: 5, last: 6 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "147" }],
-			reading_2: [{ bookId: BookId.Acts, chapters: "7-8" }],
+			ofMonth: 28,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 147, last: 147 } },
+			],
+			reading_2: [{ bookId: BookId.Acts, chapters: { first: 7, last: 8 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "148" }],
-			reading_2: [{ bookId: BookId.Acts, chapters: "9-10" }],
+			ofMonth: 29,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 148, last: 148 } },
+			],
+			reading_2: [{ bookId: BookId.Acts, chapters: { first: 9, last: 10 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "149" }],
-			reading_2: [{ bookId: BookId.Acts, chapters: "11-12" }],
+			ofMonth: 30,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 149, last: 149 } },
+			],
+			reading_2: [{ bookId: BookId.Acts, chapters: { first: 11, last: 12 } }],
 		},
 		{
+			ofMonth: 31,
 			reading_1: [
 				{
 					bookId: BookId.Samuel1,
-					chapters: "2",
-					verses: "1-10",
+					chapters: { first: 2, last: 2 },
+					verses: { first: 1, last: 10 },
 				},
 			],
-			reading_2: [{ bookId: BookId.Acts, chapters: "13-14" }],
+			reading_2: [{ bookId: BookId.Acts, chapters: { first: 13, last: 14 } }],
 		},
 	],
 };
