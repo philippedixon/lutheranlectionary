cc257f458a91b2c9023e35e35faa30c081e66579
Philippe <philippe.dixon@protonmail.com>
2025-01-12 20:38:46 -0500

    Update November readings to new interface

--- src/app/constants/lectionary/novemberReadings.ts
+++ src/app/constants/lectionary/novemberReadings.ts
@@ -5,39 +5,42 @@ export const novemberReadings: Month = {
 	name: "November",
 	days: [
 		{
+			ofMonth: 1,
 			reading_1: [
 				{
 					bookId: BookId.Exodus,
-					chapters: "15",
-					verses: "1-8",
+					chapters: { first: 15, last: 15 },
+					verses: { first: 1, last: 8 },
 				},
 			],
 			reading_2: [
 				{
 					bookId: BookId.Hosea,
-					chapters: "8-10",
+					chapters: { first: 8, last: 10 },
 				},
 			],
 		},
 		{
+			ofMonth: 2,
 			reading_1: [
 				{
 					bookId: BookId.Psalms,
-					chapters: "122",
+					chapters: { first: 122, last: 122 },
 				},
 			],
 			reading_2: [
 				{
 					bookId: BookId.Hosea,
-					chapters: "11-14",
+					chapters: { first: 11, last: 14 },
 				},
 			],
 		},
 		{
+			ofMonth: 3,
 			reading_1: [
 				{
 					bookId: BookId.Psalms,
-					chapters: "123",
+					chapters: { first: 123, last: 123 },
 				},
 			],
 			reading_2: [
@@ -47,12 +50,18 @@ export const novemberReadings: Month = {
 			],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "124" }],
-			reading_2: [{ bookId: BookId.Amos, chapters: "1-5" }],
+			ofMonth: 4,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 124, last: 124 } },
+			],
+			reading_2: [{ bookId: BookId.Amos, chapters: { first: 1, last: 5 } }],
 		},
 		{
-			reading_1: [{ bookId: BookId.Psalms, chapters: "125" }],
-			reading_2: [{ bookId: BookId.Amos, chapters: "6-9" }],
+			ofMonth: 5,
+			reading_1: [
+				{ bookId: BookId.Psalms, chapters: { first: 125, last: 125 } },
+			],
+			reading_2: [{ bookId: BookId.Amos, chapters: { first: 6, last: 9 } }],
 		},
 	],
 };
