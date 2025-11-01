3c100092fbcd53b559c8ff971dec47879ed1bb3d
Philippe <philippedixon@protonmail.com>
2025-11-01 05:35:00 -0400

    Add vertical grid to front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,30 +14,34 @@ const Home = () => {
 
 					return (
 						<div key={month.name}>
-							<h2>{month.name}</h2>
-							{month?.days?.map((day) => {
-								const date = day.ofTheMonth;
-								const reading1Display = day.firstReading
-									.map((reading) => getReadingTitle(reading))
-									.join(", ");
-								const reading2Display = day.secondReading
-									.map((reading) => getReadingTitle(reading))
-									.join(", ");
+							<h2 className="text-center">{month.name}</h2>
+							<div className="grid grid-flow-col grid-rows-[repeat(11,auto)] auto-cols-[minmax(0,12rem)] gap-4 justify-center">
+								{month?.days?.map((day) => {
+									const date = day.ofTheMonth;
+									const reading1Display = day.firstReading
+										.map((reading) => getReadingTitle(reading))
+										.join(", ");
+									const reading2Display = day.secondReading
+										.map((reading) => getReadingTitle(reading))
+										.join(", ");
 
-								return (
-									<div key={`${month.name}-${date}`}>
-										<Link href={`/${monthNumber}/${date}`}>
-											<h3 data-testid={`date:${month.name}-${date}`}>{date}</h3>
-											<p data-testid={`reading1:${month.name}-${date}`}>
-												{reading1Display}
-											</p>
-											<p data-testid={`reading2:${month.name}-${date}`}>
-												{reading2Display}
-											</p>
-										</Link>
-									</div>
-								);
-							})}
+									return (
+										<div key={`${month.name}-${date}`}>
+											<Link href={`/${monthNumber}/${date}`}>
+												<h3 data-testid={`date:${month.name}-${date}`}>
+													{date}
+												</h3>
+												<p data-testid={`reading1:${month.name}-${date}`}>
+													{reading1Display}
+												</p>
+												<p data-testid={`reading2:${month.name}-${date}`}>
+													{reading2Display}
+												</p>
+											</Link>
+										</div>
+									);
+								})}
+							</div>
 							<br />
 						</div>
 					);
