a983159098a814081232c52255e287eba4336e52
Philippe <philippedixon@protonmail.com>
2025-12-22 06:11:24 -0500

    Hide ESV option in dropdown

--- src/app/components/TranslationsDropdownServerComponent.tsx
+++ src/app/components/TranslationsDropdownServerComponent.tsx
@@ -1,16 +1,16 @@
 import { fetchAvailableTranslations } from "@/app/utils";
 import { TranslationsDropdown } from "./TranslationsDropdown";
-import { Translation } from "@/app/interfaces";
+// import { Translation } from "@/app/interfaces";
 
 export const TranslationsDropdownServerComponent = async () => {
 	const availableTranslations = await fetchAvailableTranslations();
-	const esvTranslation = {
-		id: "eng_esv",
-		languageEnglishName: "English",
-		name: "English Standard Version (ESV)",
-	} as Translation;
+	// const esvTranslation = {
+	// 	id: "eng_esv",
+	// 	languageEnglishName: "English",
+	// 	name: "English Standard Version (ESV)",
+	// } as Translation;
 
-	availableTranslations.translations.push(esvTranslation);
+	// availableTranslations.translations.push(esvTranslation);
 
 	return (
 		<TranslationsDropdown translations={availableTranslations.translations} />
