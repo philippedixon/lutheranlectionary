616680b5b60aa628c32d93989218d63dc1279653
Philippe <philippedixon@protonmail.com>
2026-01-23 06:19:29 -0500

    Temporarily comment out ESV

--- src/app/components/TranslationsDropdownServerComponent.tsx
+++ src/app/components/TranslationsDropdownServerComponent.tsx
@@ -1,17 +1,17 @@
 import { fetchAvailableTranslations } from "@/app/utils";
 import { TranslationsDropdown } from "./TranslationsDropdown";
-import { Translation } from "@/app/interfaces";
+// import { Translation } from "@/app/interfaces";
 import { translastionsBlackList } from "@/app/constants";
 
 export const TranslationsDropdownServerComponent = async () => {
 	const availableTranslations = await fetchAvailableTranslations();
-	const esvTranslation = {
-		id: "eng_esv",
-		languageEnglishName: "English",
-		name: "English Standard Version (ESV)",
-	} as Translation;
+	// const esvTranslation = {
+	// 	id: "eng_esv",
+	// 	languageEnglishName: "English",
+	// 	name: "English Standard Version (ESV)",
+	// } as Translation;
 
-	availableTranslations.translations.push(esvTranslation);
+	// availableTranslations.translations.push(esvTranslation);
 	const filteredTranslations = availableTranslations.translations.filter(
 		(translation) => !translastionsBlackList.has(translation.id),
 	);
